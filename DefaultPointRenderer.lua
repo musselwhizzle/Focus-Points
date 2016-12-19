a8df34ceaca5f35acb08ec10eec81c33201ec42b
Musselwhizzle <jm@therealjoshua.com>
2016-12-19 11:36:01 -0800

    added apache license

--- DefaultPointRenderer.lua
+++ DefaultPointRenderer.lua
@@ -1,4 +1,20 @@
 --[[
+  Copyright 2016 Joshua Musselwhite, Whizzbang Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+--]]
+
+--[[
   This object is responsible for creating the focus point icon and figuring out where to place it. 
   This logic should be universally reuseable, but if it's not, this object can be replaced in the 
   PointsRendererFactory. 
--- PointsRendererFactory.lua
+++ PointsRendererFactory.lua
@@ -1,4 +1,20 @@
 --[[
+  Copyright 2016 Joshua Musselwhite, Whizzbang Inc
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+--]]
+
+--[[
   Factory for creating the focus point renderer and getting the focus points. To expand this plugin to allow for more
   camera, just add more mapped points to the #getFocusPoints() and #getFocusPointDimens() methods
 --]]
