8ad02dd9e7558f17a6656048edd526a2d3001f23
Musselwhizzle <jm@therealjoshua.com>
2017-01-05 14:53:39 -0800

    removed duplicate methods

--- focuspoints.lrdevplugin/Utils.lua
+++ focuspoints.lrdevplugin/Utils.lua
@@ -26,6 +26,8 @@ local LrPathUtils = import 'LrPathUtils'
 local LrLogger = import 'LrLogger'
 local LrStringUtils = import "LrStringUtils"
 
+require "ExifUtils"
+
 local myLogger = LrLogger( 'libraryLogger' )
 myLogger:enable( "logfile" )
 
@@ -36,35 +38,7 @@ exiftool = LrPathUtils.child( _PLUGIN.path, "bin" )
 exiftool = LrPathUtils.child(exiftool, "exiftool")
 exiftool = LrPathUtils.child(exiftool, "exiftool")
 
-function startDebug()
-  -- no op
-end
 
-function getExifCmd(targetPhoto) 
-  
-  local path = targetPhoto:getRawMetadata("path")
-  local metaDataFile = LrPathUtils.removeExtension(path)
-  metaDataFile = metaDataFile .. "-metadata.txt"
-  
-  local cmd = "'"..exiftool .. "' -a -u -g1 '" .. path .. "' > '" .. metaDataFile .. "'";
-  return cmd, metaDataFile
-  
-end
-
-function readMetaData(targetPhoto)
-  local cmd, metaDataFile = getExifCmd(targetPhoto)
-  LrTasks.execute(cmd)
-  local fileInfo = LrFileUtils.readFile(metaDataFile)
-  LrFileUtils.delete(metaDataFile)
-  return fileInfo
-end
-
-function filterInput(str)
-  --local result = string.gsub(str, "[^a-zA-Z0-9 ,\\./;'\\<>\\?:\\\"\\{\\}\\|!@#\\$%\\^\\&\\*\\(\\)_\\+\\=-\\[\\]~`]", "?");
-  -- FIXME: doesn't strip - or ] correctly
-  local result = string.gsub(str, "[^a-zA-Z0-9 ,\\./;'\\<>\\?:\\\"\\{\\}\\|!@#\\$%\\^\\&\\*\\(\\)_\\+\\=\\-\\[\\\n\\\t~`-]", "?");
-  return result
-end
 
 function splitText(str, delim)
   if str == nill then return nill end
