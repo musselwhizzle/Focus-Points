6d6456abe0a0b8d6f6f56e314fe914d74029a019
Jan D'Hollander <jan.dhollander@chapoo.com>
2017-01-05 21:25:54 +0100

    Added windows support

--- focuspoints.lrdevplugin/PointsRendererFactory.lua
+++ focuspoints.lrdevplugin/PointsRendererFactory.lua
@@ -27,15 +27,17 @@ local LrErrors = import 'LrErrors'
 PointsRendererFactory = {}
 
 function PointsRendererFactory.createRenderer(photo)
-  local cameraMake = photo:getFormattedMetadata("cameraMake")
-  local cameraModel = photo:getFormattedMetadata("cameraModel")
+  local cameraMake = PointsRendererFactory.getCameraMake(photo)
+  local cameraModel = PointsRendererFactory.getCameraModel(photo)
   
   -- change the metadata names here
-  if (string.lower(cameraMake) == "ricoh imaging company, ltd." and string.lower(cameraModel) == "pentax k-1") then
+  if (cameraMake == "ricoh imaging company, ltd." and cameraModel == "pentax k-1") then
     DefaultDelegates.metaKeyAfPointUsed = "AF Points Selected"
+  elseif (cameraMake == "canon") then
+    DefaultDelegates.metaKeyAfPointUsed = "AF Points In Focus"
   end
   
-  if (cameraMake == "FUJIFILM") then
+  if (cameraMake == "fujifilm") then
       DefaultPointRenderer.funcGetAFPixels = FujiDelegates.getFujiAfPoints
       DefaultPointRenderer.focusPointDimen = {1,1} -- this is wrong. it's probably more like 300,250
   else 
@@ -51,14 +53,22 @@ function PointsRendererFactory.createRenderer(photo)
 end
 
 function PointsRendererFactory.getFocusPoints(photo)
-  local cameraMake = photo:getFormattedMetadata("cameraMake")
-  local cameraModel = photo:getFormattedMetadata("cameraModel")
+  local cameraMake = PointsRendererFactory.getCameraMake(photo)
+  local cameraModel = PointsRendererFactory.getCameraModel(photo)
   
-  local focusPoints, focusPointDimens =  PointsUtils.readIntoTable(string.lower(cameraMake), string.lower(cameraModel) .. ".txt")
+  local focusPoints, focusPointDimens =  PointsUtils.readIntoTable(cameraMake, cameraModel .. ".txt")
   
   if (focusPoints == nil) then
     return "No (or incorrect) mapping found at: \n" .. string.lower(cameraMake) .. "/" .. string.lower(cameraModel) .. ".txt"
   else 
     return focusPoints, focusPointDimens
   end
+end
+
+function PointsRendererFactory.getCameraMake(photo)
+  return string.lower(photo:getFormattedMetadata("cameraMake"))
+end
+
+function PointsRendererFactory.getCameraModel(photo)
+  return string.lower(photo:getFormattedMetadata("cameraModel"))
 end
