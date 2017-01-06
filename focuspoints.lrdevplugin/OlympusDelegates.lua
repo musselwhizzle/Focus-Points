06e2dff8ac66ac31067eecd7eda7bea6cb45b860
mkjanke <me@Me-Air.local>
2017-01-06 08:39:52 -0600

    Added models E-M5MarkII, E-M10MarkII

--- focuspoints.lrdevplugin/OlympusDelegates.lua
+++ focuspoints.lrdevplugin/OlympusDelegates.lua
@@ -17,7 +17,7 @@
 --[[
   A collection of delegate functions to be passed into the DefaultPointRenderer when
   the camera is Olympus
-  E-M1, E-M1 II metadata is like:
+  E-M1, E-M1 II focus point metadata:
   
     AF Areas                        : (118,32)-(137,49)
     AF Point Selected               : (50%,15%) (50%,15%)
@@ -27,11 +27,12 @@
         AF Areas appears to be focus box as coordinates relative to 0..255 from upper left corner (x,y)
         
         
-  2017-01-05 - MJ - E-M1 family only
+  2017-01-06 - MJ - E-Mn family only
   
 --]]
 
 local LrStringUtils = import "LrStringUtils"
+local LrErrors = import 'LrErrors'
 require "Utils"
 
 OlympusDelegates = {}
@@ -42,16 +43,16 @@ OlympusDelegates = {}
 function OlympusDelegates.getOlympusAfPoints(photo, metaData)
     local cameraModel = photo:getFormattedMetadata("cameraModel")
     
-  if (cameraModel == "E-M1") or (cameraModel == "E-M1MarkII" )then
+  if (cameraModel == "E-M1") or (cameraModel == "E-M1MarkII")  or (cameraModel == "E-M10MarkII") or (cameraModel == "E-M5MarkII") then
     local focusPoint = ExifUtils.findValue(metaData, "AF Point Selected")
     local focusX, focusY = string.match(focusPoint, "%((%d+)%%,(%d+)")
-    if focusX == nil or focusY == nil then return nil end
     log ("Focus %: " .. focusX .. "," ..  focusY .. "," .. focusPoint  )
+    if focusX == nil or focusY == nil then return nil, nil end
   
     local dimens = photo:getFormattedMetadata("dimensions")
     orgPhotoW, orgPhotoH = parseDimens(dimens)
-    if orgPhotoW == nil or orgPhotoH == nil then return nil end
     log ( "orgPhotoW: " .. orgPhotoW .. " orgPhotoH: ".. orgPhotoH )
+    if orgPhotoW == nil or orgPhotoH == nil then return nil, nil end
   
     log ( "Focus px: " .. tonumber(orgPhotoW) * tonumber(focusX)/100 .. "," .. tonumber(orgPhotoH) * tonumber(focusY)/100)
  
@@ -60,6 +61,8 @@ function OlympusDelegates.getOlympusAfPoints(photo, metaData)
     else
         return  tonumber(orgPhotoW) * tonumber(focusY)/100, tonumber(orgPhotoH) * tonumber(focusX)/100
     end
-    
+  else
+    LrErrors.throwUserError("Unsupported Olympus Camera.")
+    return nil, nil    
   end
 end
